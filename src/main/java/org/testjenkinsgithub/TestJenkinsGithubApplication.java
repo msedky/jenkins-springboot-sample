238478232b31d9112307590ead967ffc4064a969
msedky <m.hosamsedky@hotmail.com>
2023-09-29 19:48:42 +0300

    first-commit

---
+++ src/main/java/org/testjenkinsgithub/TestJenkinsGithubApplication.java
@@ -0,0 +1,24 @@
+package org.testjenkinsgithub;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkinsGithubApplication implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestJenkinsGithubApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("it is a continous integration job");
+		SpringApplication.run(TestJenkinsGithubApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("it is my second logger, args = " + args);
+	}
+
+}
---
+++ src/test/java/org/testjenkinsgithub/TestJenkinsGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.testjenkinsgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
